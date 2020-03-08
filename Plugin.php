b6504124a0be19fab56f005d437508256ee2053d
Zach Schneider <hello@zacharyschneider.ca>
2020-03-07 16:11:04 -0800

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Zaxbux\SecurityHeaders;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Security Headers',
+        'description' => ''
+    ]
+];
