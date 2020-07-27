a390fd580fe65448b3696f45921d35ea493637a9
Zach Schneider <hello@zacharyschneider.ca>
2020-07-27 11:57:18 -0700

    Add middleware

--- routes.php
+++ routes.php
@@ -3,4 +3,4 @@
 Route::post('/_/reports/csp-endpoint/{action}', [
 	'as'   => 'zaxbux.securityheaders.reports.csp_endpoint',
 	'uses' => 'Zaxbux\SecurityHeaders\Http\Controllers\ReportsController@cspEndpoint'
-])->middleware('web');
+])->middleware(['Zaxbux\SecurityHeaders\Classes\CSPEndpointMiddleware']);
