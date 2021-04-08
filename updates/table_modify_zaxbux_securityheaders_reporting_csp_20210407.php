dab96e5c180f305bdb1a0ba512fd86bd8bb1a7b1
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-07 23:24:24 -0700

    class name duplicated

--- updates/table_modify_zaxbux_securityheaders_reporting_csp_20210407.php
+++ updates/table_modify_zaxbux_securityheaders_reporting_csp_20210407.php
@@ -3,7 +3,7 @@
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class TableCreateZaxbuxSecurityheadersReportingCsp extends Migration
+class TableCreateZaxbuxSecurityheadersReportingCsp20210407 extends Migration
 {
     public function up()
     {
